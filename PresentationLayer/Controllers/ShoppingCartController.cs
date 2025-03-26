6c82ab45479d26c70fa66edac051213753f57063
Mazen Elnems <mazenelnems@gmail.com>
2025-03-26 23:12:11 +0200

    complete shoppingCart features

--- PresentationLayer/Controllers/ShoppingCartController.cs
+++ PresentationLayer/Controllers/ShoppingCartController.cs
@@ -57,6 +57,38 @@ namespace PresentationLayer.Controllers
             return View(cart);
         }
 
+        public IActionResult Plus(int id)
+        {
+            ShoppingCartVM? cart = HttpContext.Session.Get<ShoppingCartVM>("Cart");
+            ShoppingCartItemVM? cartItem = cart?.CartItems.FirstOrDefault(x => x.ProductId == id);
+            if (cartItem != null && cart!= null)
+            {
+                cartItem.Quantity++;
+                cartItem.SubTotal = cartItem.Quantity * cartItem.Price;
+                cart.Total = cart.CartItems.Sum(x => x.SubTotal);
+
+                HttpContext.Session.Set("Cart", cart);
+            }
+            return RedirectToAction(nameof(ViewCart));
+        }
+
+        public IActionResult Minus(int id)
+        {
+            ShoppingCartVM? cart = HttpContext.Session.Get<ShoppingCartVM>("Cart");
+            ShoppingCartItemVM? cartItem = cart?.CartItems.FirstOrDefault(x => x.ProductId == id);
+            if (cartItem != null && cart != null)
+            {
+                cartItem.Quantity--;
+                if (cartItem.Quantity > 0)
+                {
+                    cartItem.SubTotal = cartItem.Quantity * cartItem.Price;
+                }
+                cart.Total = cart.CartItems.Sum(x => x.SubTotal);
+                HttpContext.Session.Set("Cart", cart);
+            }
+            return RedirectToAction(nameof(ViewCart));
+        }
+
         public IActionResult Remove(int id)
         {
             var cart = HttpContext.Session.Get<ShoppingCartVM>("Cart");
